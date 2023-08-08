0a521bf6a492d1c68e9118ecaa0a074965f5bed3
Hyba <hibazoglami2018@gmail.com>
2023-08-08 13:59:11 +0200

    Done

--- Solo Projects .js
+++ Solo Projects .js
@@ -1,77 +1,13 @@
 
-var products = [
-    {
-      image: 'c:\Users\hibaz\Desktop\RBK\Solo-Projects- Hybaa\photo\pushpak-dsilva-r-hQw_obFd0-unsplash.jpg',
-    },
-    {
-      image: 'c:\Users\hibaz\Desktop\RBK\Solo-Projects- Hybaa\photo\emy-Rx3QSrG1coc-unsplash.jpg',
-    },
-  ];
-  function createProductCards(products) {
-    var productGrid = document.querySelector('.product-grid');
-  
-    products.forEach(product => {
-      var card = document.createElement('div');
-      card.classList.add('product-card');
-  
-      var image = document.createElement('img');
-      image.src = product.image;
-      image.alt = product.name + ' Image';
-      image.classList.add('product-image');
-  
-      var name = document.createElement('h3');
-      name.textContent = product.name;
-  
-      var priceTag = document.createElement('p');
-      priceTag.textContent = '$' + product.price;
-  
-      card.appendChild(image);
-      card.appendChild(name);
-      card.appendChild(priceTag);
-      image.addEventListener('mouseenter', function() {
-        image.src = 'c:\Users\hibaz\Desktop\RBK\Solo-Projects- Hybaa\photo\chocolate-banner.jpg';
-        image.src = 'c:\Users\hibaz\Desktop\RBK\Solo-Projects- Hybaa\photo\egor-lyfar-DPNrBT1WCMs-unsplash.jpg" alt="Chocolate Product 2';
-        image.src = 'c:\Users\hibaz\Desktop\RBK\Solo-Projects- Hybaa\photo\american-heritage-chocolate-TZFshUFzWRQ-unsplash.jpg" alt="Chocolate Product 2';
-      });
-  
-      image.addEventListener('mouseleave', function() {
-        image.src = product.image;
-      });
-  
-      card.appendChild(image);
-      card.appendChild(name);
-      card.appendChild(priceTag);
-  
-      productGrid.appendChild(card);
+$(document).ready(function() {
+    $('.book-button').click(function() {
+      var productName = $(this).siblings('h3').text();
+      var productPrice = $(this).siblings('p').text();
+      
+      
+      var confirmationMessage = "Do you want to sale " + productName + " for " + productPrice + "?";
+      if (confirm(confirmationMessage)) {
+        alert("Product booked!");
+      }
     });
-  }
-var productsData = [
-    { name: "Dark Chocolate", price: 3.99, image: "c:\Users\hibaz\Desktop\RBK\Solo-Projects- Hybaa\photo\chocolate-product2.jpg" },
-    { name: "Milk Chocolate", price: 2.99, image: "c:\Users\hibaz\Desktop\RBK\Solo-Projects- Hybaa\photo\pushpak-dsilva-r-hQw_obFd0-unsplash.jpg" },
-    { name: "White Chocolate", price: 4.49, image: "c:\Users\hibaz\Desktop\RBK\Solo-Projects- Hybaa\photo\emy-Rx3QSrG1coc-unsplash.jpg" }
-];
-
-
-function createProductCard(imageSrc, productName, price) {
-    var productGrid = document.querySelector('.product-grid');
-
-    var card = document.createElement('div');
-    card.classList.add('product-card');
-
-    var image = document.createElement('img');
-    image.src = imageSrc;
-    image.alt = productName + ' Image';
-    image.classList.add('product-image');
-
-    var name = document.createElement('h3');
-    name.textContent = productName;
-
-    var priceTag = document.createElement('p');
-    priceTag.textContent = '$' + price;
-
-    card.appendChild(image);
-    card.appendChild(name);
-    card.appendChild(priceTag);
-
-    productGrid.appendChild(card);
-  }
+  });
